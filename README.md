99a027b97854a96de0083e721f26ef4d35e047f3
Nasaee <nasaee.dev@gmail.com>
2023-08-30 12:47:28 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Notes
+
+https://react-course-comfy-sloth-store.netlify.app/
+https://github.com/john-smilga/react-course-comfy-sloth-store
+auth web: https://auth0.com/
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
+import { CartProvider } from './context/cart_context';
+import { UserProvider } from './context/user_context';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain='dev-cwqld7aacet6gyjn.us.auth0.com'
+    clientId='xZdo7XfiUaXBH8rhCuugXnaDaUYsxAiz'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+);
